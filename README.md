51315c83862f2cd2fc10c374cc8437bc1bcace9e
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2013-05-21 19:39:01 +0200

    update

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-deCasteljau
-===========
-
 JavaScript implementation of de Casteljau algorithm.
+
+##Example of use
+```html
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>De Casteljau algorithm.</title>
+</head>
+<body>
+</body>
+<script src="decasteljau.js" async></script>
+<script>
+        window.addEventListener("load", function (e) {
+            var canvas = document.createElement("canvas");
+            canvas.width = 600;
+            canvas.height = 600;
+
+            document.body.appendChild(canvas);
+
+            var ctx = canvas.getContext("2d");
+
+            ctx.strokeStyle = "black";
+            ctx.fillStyle = "red";
+
+            ctx.beginPath();
+            ctx.moveTo(200,200);
+            ctx.bezierCurveTo(200, 100, 300, 400, 400,400);
+            ctx.stroke();
+
+            var point = de.casteljau([
+                //control points
+                [200, 200],
+                [200, 100],
+                [300, 400],
+                [400, 400]
+            ], 0.8);
+
+            ctx.beginPath();
+            ctx.moveTo(point[0], point[1]);
+            ctx.arc(point[0], point[1], 2, 0, 2 * Math.PI);
+            ctx.fill();
+        });   
+    </script>
+</html>
+```
+
+##License
+MIT license
---
+++ decasteljau.js
@@ -0,0 +1,57 @@
+(function (global) {
+    /**
+     * Recursive implementation of de Casteljau algorithm.
+     * If you want to know more about algorith itself check out
+     * Wikipedia:
+     * {@link [symbol]}
+     * 
+     * @param  {array} points   array of control points of Bezier curve
+     * @param  {double} t       value between 0 and 1 - 0 is for the beginning of the curve, 1 is for the end
+     * @return {array}          point on Bezier curve, index 0 is x coord, index 1 is y coord
+     */
+    function deCasteljauAlgorithm (points, t) {
+        if (t === 1) {
+            return points[points.length - 1];
+        }
+
+        if (t === 0) {
+            return points[0];
+        }
+
+        if (points.length == 1) {
+            return points[0];
+        }
+
+        var calculatedPoints = [];
+
+        for (var i = 1, len = points.length; i < len; i++) {
+            calculatedPoints.push(calculatePoints([points[i - 1], points[i]], t));
+        }
+
+        return deCasteljauAlgorithm(calculatedPoints, t);
+    }
+
+    /**
+     * Helper function calculating new point between two given points.
+     * The new point is t of the distance between given points.
+     * 
+     * @param  {array} points
+     * @param  {double} t
+     * @return {array}
+     */
+    function calculatePoints (points, t) {
+        var p1X = points[0][0], //x coord of first point
+            p1Y = points[0][1], //y coord of first point
+            p2X = points[1][0], //x coord of second point
+            p2Y = points[1][1]; //y coord of second point
+
+        var pInterX = p1X + (p2X - p1X) * t,
+            pInterY = p1Y + (p2Y - p1Y) * t;
+
+        return [pInterX, pInterY];
+    }
+
+    global["de"] = {
+        casteljau: deCasteljauAlgorithm
+    };
+})(this);
